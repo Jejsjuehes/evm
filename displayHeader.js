83c5936ec796b418217cf3545d4d95f9df222206
Jejsjuehes <hed8reaofficial@gmail.com>
2025-09-06 12:47:54 +0530

    Add files via upload

---
+++ displayHeader.js
@@ -0,0 +1,7 @@
+const colors = require("colors");
+
+function displayHeader() {
+  console.log(colors.yellow("Tool được phát triển bởi nhóm telegram: https://t.me/airdrophuntersieutoc"));
+}
+
+module.exports = displayHeader;
