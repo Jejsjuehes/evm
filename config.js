c117c62cc2ab38221ae2ab628693d6623574d6a3
Jejsjuehes <hed8reaofficial@gmail.com>
2025-09-06 12:54:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 const config = {
-  amount: [0.0002, 0.0001], // random amount ETH | số lượng ETH gửi tới các ví, random ngẫu nhiên
+  amount: [0.05, 0.04], // random amount ETH | số lượng ETH gửi tới các ví, random ngẫu nhiên
   number_transactions: 1, // number of transactions each wallet | số giao dịch thực hiện trên mỗi ví
 };
 
 module.exports = { config };
+
